83d779e14d4a7cd3080baac1d18b4c2f94a8be58
Destroyer616 <saikiran.reddy916@gmail.com>
2024-10-20 14:09:27 +0530

    scripts to manage containers

--- start.sh
+++ start.sh
@@ -8,3 +8,6 @@ docker-compose -f ./jenkins/jenkins-compose.yml up -d
 
 # Compose up nexus
 docker-compose -f ./nexus/nexus-compose.yml up -d
+
+# Compose up sonarqube
+docker-compose -f ./sonarqube/sonarqube-compose.yml up -d
--- stop.sh
+++ stop.sh
@@ -5,3 +5,6 @@ docker-compose -f ./jenkins/jenkins-compose.yml down
 
 # Compose down nexus
 docker-compose -f ./nexus/nexus-compose.yml down
+
+# Compose down sonarqube
+docker-compose -f ./sonarqube/sonarqube-compose.yml down
